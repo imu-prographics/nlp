1474cfe8833ffd30f2131ceb72fd373ea0fa9afa
NISHIUMI Yusuke(is0368pi) <is0368pi@ed.ritsumei.ac.jp>
2019-10-09 20:38:02 +0900

    省メモリ版完成

--- LSTM_proofreading.py
+++ LSTM_proofreading.py
@@ -16,126 +16,129 @@ import glob
 import json
 import MeCab
 
-
-def parser():
-    usage = 'Usage: python --file weight_file_name --mode [train or test]'
-    argparser = ArgumentParser(usage=usage)
-    argparser.add_argument('--mode','-m', dest='mode', type=str, choices=['train','test'])
-    argparser.add_argument('--weights', '-w', type=str, required=True, help='set filename of weights for save or load')
-    argparser.add_argument('--load_model', '-l', type=str, required=True, help='set filename of load_model for save or load')
-    argparser.add_argument('--input', '-i', type=str, required=False, help='set filename of input data path')
-    argparser.add_argument('--target', '-t', type=str, required=False, help='set filename of target data path')
-    argparser.add_argument('--num_samples', '-s', type=int, default=1000, required=False, help='max samples for training')
-    argparser.add_argument('--epochs', '-e', type=int, default=100, required=False, help='set number of epochs')
-    args = argparser.parse_args()
-    return args
+class LSTM_proofreading(self):
+    def __init__(self):
 
 
-def build_model(maxlen=None, out_dim=None, in_dim=256):
-    with tf.device("/cpu:0"):
-        print('Build model...')
-        model = Sequential()
-        model.add(Embedding(input_dim=in_dim, output_dim=out_dim,mask_zero=True))
-        model.add(LSTM(128*20, return_sequences=False))
-        model.add(BN())
-        model.add(Dense(out_dim))
-        model.add(Activation('linear'))
-        model.add(Activation('sigmoid'))
-        #model.add(Activation('softmax'))
-        optimizer = Adam()
-        model.compile(loss='binary_crossentropy', optimizer=optimizer) 
-    return model
-
-def create_dictionary(input_data_path,target_data_path, num_samples):
-    input_texts = []
-    target_texts = []
-    dict_words = set()  # 追記
-    with  open(input_data_path, 'r', encoding='utf-8') as f:
-        input_lines = f.read().split('\n')  # 行ごとのリストに
-
-    with open(target_data_path, 'r', encoding='utf-8') as f:
-        target_lines = f.read().split('\n')
-
-    min_samples = min(num_samples, min(len(input_lines)-1, len(target_lines)-1))
-    for index, (input_text,target_text) in enumerate(zip(input_lines[:min_samples],target_lines[:min_samples])):
-        # \tが開始記号で\nが終端記号とする
-        target_text = '\t ' + target_text + ' \n'
-        # 単語単位に分割
-        seq = []
-        seq = input_text.split(' ')
-        input_texts.append(seq)
-        
-        for word in seq:
-            if word not in dict_words:
-                dict_words.add(word)
-        seq = target_text.split(' ')
-        target_texts.append(seq)
-    dict_words.add('MASK')
-    dict_words = sorted(list(dict_words))
-    num_words = len(dict_words)
-
-    # 入力文と出力文それぞれで最大単語数計算
-    max_input_seq_length = max([len(words) for words in input_texts])
-    max_target_seq_length = max([len(words) for words in target_texts])
-
-    print('Number of samples:', len(input_texts))
-    print('Number of unique words:', num_words)
-    print('Max sequence length for inputs:', max_input_seq_length)
-    print('Max sequence length for outputs:', max_target_seq_length)
-        # 単語にIDを割り振る
-    word_index = dict(
-        [(word, i) for i, word in enumerate(dict_words)])
-    #文章を単語IDの配列で表す
-    input_texts_id = list(map(lambda word: word_index(word) ,input_texts))
-    target_texts_id = list(map(lambda word: word_index(word) ,target_texts))
-    return word_index, input_texts_id, target_texts_id
-
-def split_train_val(data_list, rate):
-    n_split = int(data_list[0]*rate)
-    data_train, data_val = np.vsplit(np.array(data_list),[n_split]).tolist()
-    return data_train, data_val
+    def parser():
+        usage = 'Usage: python --file weight_file_name --mode [train or test]'
+        argparser = ArgumentParser(usage=usage)
+        argparser.add_argument('--mode','-m', dest='mode', type=str, choices=['train','test'])
+        argparser.add_argument('--weights', '-w', type=str, required=True, help='set filename of weights for save or load')
+        argparser.add_argument('--load_model', '-l', type=str, required=True, help='set filename of load_model for save or load')
+        argparser.add_argument('--input', '-i', type=str, required=False, help='set filename of input data path')
+        argparser.add_argument('--target', '-t', type=str, required=False, help='set filename of target data path')
+        argparser.add_argument('--num_samples', '-s', type=int, default=1000, required=False, help='max samples for training')
+        argparser.add_argument('--epochs', '-e', type=int, default=100, required=False, help='set number of epochs')
+        args = argparser.parse_args()
+        return args
 
 
-def ppx(y_true, y_pred):
-    loss = categorical_crossentropy(y_true, y_pred)
-    perplexity = K.cast(K.pow(math.e, K.mean(loss, axis=-1)), K.floatx())
-    return perplexity
+    def build_model(maxlen=None, out_dim=None, in_dim=256):
+        with tf.device("/cpu:0"):
+            print('Build model...')
+            model = Sequential()
+            model.add(Embedding(input_dim=in_dim, output_dim=out_dim,mask_zero=True))
+            model.add(LSTM(128*20, return_sequences=False))
+            model.add(BN())
+            model.add(Dense(out_dim))
+            model.add(Activation('linear'))
+            model.add(Activation('sigmoid'))
+            #model.add(Activation('softmax'))
+            optimizer = Adam()
+            model.compile(loss='binary_crossentropy', optimizer=optimizer) 
+        return model
 
-def train(model, epochs, batch_size, input_train, input_val, target_train, target_val):
-    row_train = input_train.shape[0]
-    row_val = target_val.shape[0]
-    n_batch = math.ceil(row_train/batch_size)
-    loss_bk = 10000
-    for j in range(0, epochs) :
-        print("Epoch ", j+1, "/", epochs)
-        loss_mean = 0
-        k = 0
-        for i in range(0, n_batch):
-            start_train = i * batch_size
-            end_train = min([(i+1)*batch_size, row_train])
-            start_val = k*batch_size
-            end_val = min([(k+1)*batch_size, row_val])
-            input_train_batch = input_train[start_train:end_train,:]
-            target_train_batch = target_train[start_train:end_train,:]
-            input_val_batch = input_val[start_val:end_val,:]
-            target_val_batch = target_val[start_val:end_val,:]
-            if end_val%row_val==0:
-                k = 0
+    def create_dictionary(input_data_path,target_data_path, num_samples):
+        input_texts = []
+        target_texts = []
+        dict_words = set()  # 追記
+        with  open(input_data_path, 'r', encoding='utf-8') as f:
+            input_lines = f.read().split('\n')  # 行ごとのリストに
+
+        with open(target_data_path, 'r', encoding='utf-8') as f:
+            target_lines = f.read().split('\n')
+
+        min_samples = min(num_samples, min(len(input_lines)-1, len(target_lines)-1))
+        for index, (input_text,target_text) in enumerate(zip(input_lines[:min_samples],target_lines[:min_samples])):
+            # \tが開始記号で\nが終端記号とする
+            target_text = '\t ' + target_text + ' \n'
+            # 単語単位に分割
+            seq = []
+            seq = input_text.split(' ')
+            input_texts.append(seq)
+            
+            for word in seq:
+                if word not in dict_words:
+                    dict_words.add(word)
+            seq = target_text.split(' ')
+            target_texts.append(seq)
+        dict_words.add('MASK')
+        dict_words = sorted(list(dict_words))
+        num_words = len(dict_words)
+
+        # 入力文と出力文それぞれで最大単語数計算
+        max_input_seq_length = max([len(words) for words in input_texts])
+        max_target_seq_length = max([len(words) for words in target_texts])
+
+        print('Number of samples:', len(input_texts))
+        print('Number of unique words:', num_words)
+        print('Max sequence length for inputs:', max_input_seq_length)
+        print('Max sequence length for outputs:', max_target_seq_length)
+            # 単語にIDを割り振る
+        word_index = dict(
+            [(word, i) for i, word in enumerate(dict_words)])
+        #文章を単語IDの配列で表す
+        input_texts_id = list(map(lambda word: word_index(word) ,input_texts))
+        target_texts_id = list(map(lambda word: word_index(word) ,target_texts))
+        return word_index, input_texts_id, target_texts_id
+
+    def split_train_val(data_list, rate):
+        n_split = int(data_list[0]*rate)
+        data_train, data_val = np.vsplit(np.array(data_list),[n_split]).tolist()
+        return data_train, data_val
+
+
+    def ppx(y_true, y_pred):
+        loss = categorical_crossentropy(y_true, y_pred)
+        perplexity = K.cast(K.pow(math.e, K.mean(loss, axis=-1)), K.floatx())
+        return perplexity
+
+    def train(model, epochs, batch_size, input_train, input_val, target_train, target_val):
+        row_train = input_train.shape[0]
+        row_val = target_val.shape[0]
+        n_batch = math.ceil(row_train/batch_size)
+        loss_bk = 10000
+        for j in range(0, epochs) :
+            print("Epoch ", j+1, "/", epochs)
+            loss_mean = 0
+            k = 0
+            for i in range(0, n_batch):
+                start_train = i * batch_size
+                end_train = min([(i+1)*batch_size, row_train])
+                start_val = k*batch_size
+                end_val = min([(k+1)*batch_size, row_val])
+                input_train_batch = input_train[start_train:end_train,:]
+                target_train_batch = target_train[start_train:end_train,:]
+                input_val_batch = input_val[start_val:end_val,:]
+                target_val_batch = target_val[start_val:end_val,:]
+                if end_val%row_val==0:
+                    k = 0
+                else:
+                    k += 1
+                train_loss = model.train_on_batch([input_train_batch,target_train_batch])
+                val_loss = model.test_on_batch([input_val_batch, target_val_batch])
+                loss_mean += val_loss
+                print("%d/%d train_loss:%f val_loss:%f" % (start_train, row_train, train_loss, val_loss))
+            loss_mean = loss_mean / n_batch
+            if j == 0 or loss_mean <= loss_bk:
+                loss_bk = loss_mean
             else:
-                k += 1
-            train_loss = model.train_on_batch([input_train_batch,target_train_batch])
-            val_loss = model.test_on_batch([input_val_batch, target_val_batch])
-            loss_mean += val_loss
-            print("%d/%d train_loss:%f val_loss:%f" % (start_train, row_train, train_loss, val_loss))
-        loss_mean = loss_mean / n_batch
-        if j == 0 or loss_mean <= loss_bk:
-            loss_bk = loss_mean
-        else:
-            print('EarlyStopping')
-    
-    json_string = model.to_json()
-    open(os.path.join(dir_model, model_filename), 'w').write(json_string)
-    model.save_weights(weights_filename)
+                print('EarlyStopping')
+        
+        json_string = model.to_json()
+        open(os.path.join(dir_model, model_filename), 'w').write(json_string)
+        model.save_weights(weights_filename)
 
 def main():
     args = parser()
--- seq2seq_proofreading.py
+++ seq2seq_proofreading.py
@@ -18,7 +18,7 @@ def parser():
     argparser = ArgumentParser(usage=usage)
     argparser.add_argument('--mode','-m', dest='mode', type=str, choices=['train','test'])
     argparser.add_argument('--weights', '-w', type=str, required=True, help='set filename of weights for save or load')
-    argparser.add_argument('--load_model', '-l', type=str, required=True, help='set filename of load_model for save or load')
+    #argparser.add_argument('--load_model', '-l', type=str, required=False, help='set filename of load_model for save or load')
     argparser.add_argument('--input', '-i', type=str, required=False, help='set filename of input data path')
     argparser.add_argument('--target', '-t', type=str, required=False, help='set filename of target data path')
     argparser.add_argument('--num_samples', '-s', type=int, default=1000, required=False, help='max samples for training')
@@ -32,7 +32,7 @@ def ppx(y_true, y_pred):
 
 args = parser()
 
-model_filename = args.load_model
+#model_filename = args.load_model
 weights_filename = args.weights
 
 gpu_count = 2
@@ -45,112 +45,116 @@ num_samples = args.num_samples  # Number of samples to train on.
 input_data_path = args.input
 target_data_path = args.target
 # 単語の配列で表された文の配列に変更
+
+input_texts = []
+target_texts = []
+
+input_words = set()  # 追記
+target_words = set()  # 追記
+with  open(input_data_path, 'r', encoding='utf-8') as f:
+    input_lines = f.read().split('\n')  # 行ごとのリストに
+
+with open(target_data_path, 'r', encoding='utf-8') as f:
+    target_lines = f.read().split('\n')
+
+min_samples = min(num_samples, min(len(input_lines)-1, len(target_lines)-1))
+for index, (input_text,target_text) in enumerate(zip(input_lines[:min_samples],target_lines[:min_samples])):
+    # \tが開始記号で\nが終端記号とする
+    target_text = '\t ' + target_text + ' \n'
+
+    # 単語単位に分割
+    words = []
+    words = input_text.split(' ')
+    input_texts.append(words)
+    for word in words:
+        if word not in input_words:
+            input_words.add(word)
+    
+    words = target_text.split(' ')
+    target_texts.append(words)
+    for word in words:
+        if word not in target_words:
+            target_words.add(word)
+
+input_words = sorted(list(input_words))
+target_words = sorted(list(target_words))
+num_encoder_words = len(input_words)
+num_decoder_words = len(target_words)
+
+# 入力文と出力文それぞれで最大単語数計算
+max_encoder_seq_length = max([len(words) for words in input_texts])
+max_decoder_seq_length = max([len(words) for words in target_texts])
+
+print('Number of samples:', len(input_texts))
+print('Number of unique input words:', num_encoder_words)
+print('Number of unique output words:', num_decoder_words)
+print('Max sequence length for inputs:', max_encoder_seq_length)
+print('Max sequence length for outputs:', max_decoder_seq_length)
+
+# 単語にIDを割り振る
+input_word_index = dict(
+    [(word, i) for i, word in enumerate(input_words)])
+target_word_index = dict(
+    [(word, i) for i, word in enumerate(target_words)])
+
+#input_textsは文の配列なので、単語の配列で構成された文の配列が必要？
+#one-hot encodingになってるので分散表現に変更したい]
+# サンプル数＊文の最大単語数＊単語の種類
+encoder_input_data = np.zeros(
+    (len(input_texts), max_encoder_seq_length, num_encoder_words),
+    dtype='uint8')
+decoder_input_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_words),
+    dtype='uint8')
+decoder_target_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_words),
+    dtype='uint8')
+
+# 文のインデックスと単語のインデックスと単語を格納するデータを作成
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    for t, word in enumerate(input_text):
+        encoder_input_data[i, t, input_word_index[word]] = 1
+    for t, word in enumerate(target_text):
+        # decoder_target_data is ahead of decoder_input_data by one timestep
+        decoder_input_data[i, t, target_word_index[word]] = 1
+        if t > 0:
+            # decoder_target_data will be ahead by one timestep
+            # and will not include the start character.
+            decoder_target_data[i, t - 1, target_word_index[word]] = 1
+
+# Define an input sequence and process it.
+with tf.device("/cpu:0"):
+    encoder_inputs = Input(shape=(None, num_encoder_words))
+    encoder = LSTM(latent_dim, return_state=True)
+    _, state_h, state_c = encoder(encoder_inputs)
+    # We discard `encoder_outputs` and only keep the states.
+    encoder_states = [state_h, state_c]
+
+    # Set up the decoder, using `encoder_states` as initial state.
+    decoder_inputs = Input(shape=(None, num_decoder_words))
+    # We set up our decoder to return full output sequences,
+    # and to return internal states as well. We don't use the
+    # return states in the training model, but we will use them in inference.
+    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                        initial_state=encoder_states)
+    decoder_dense = Dense(num_decoder_words, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
+
+    # Define the model that will turn
+    # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+n_split = int(encoder_input_data.shape[0]*0.8)
+encoder_train, encoder_val = np.vsplit(encoder_input_data,[n_split])   #エンコーダインプットデータを訓練用と評価用に分割
+decoder_train, decoder_val = np.vsplit(decoder_input_data,[n_split])   #デコーダインプットデータを訓練用と評価用に分割
+target_train, target_val = np.vsplit(decoder_target_data,[n_split])   #ラベルデータを訓練用と評価用に分割
+
+# Run training
+
+model = multi_gpu_model(model, gpus=gpu_count)
+model.compile(optimizer='rmsprop', loss=ppx) 
 if args.mode == 'train':
-    input_texts = []
-    target_texts = []
-
-    input_words = set()  # 追記
-    target_words = set()  # 追記
-    with  open(input_data_path, 'r', encoding='utf-8') as f:
-        input_lines = f.read().split('\n')  # 行ごとのリストに
-
-    with open(target_data_path, 'r', encoding='utf-8') as f:
-        target_lines = f.read().split('\n')
-
-    min_samples = min(num_samples, min(len(input_lines)-1, len(target_lines)-1))
-    for index, (input_text,target_text) in enumerate(zip(input_lines[:min_samples],target_lines[:min_samples])):
-        # \tが開始記号で\nが終端記号とする
-        target_text = '\t ' + target_text + ' \n'
-
-        # 単語単位に分割
-        words = []
-        words = input_text.split(' ')
-        input_texts.append(words)
-        for word in words:
-            if word not in input_words:
-                input_words.add(word)
-        
-        words = target_text.split(' ')
-        target_texts.append(words)
-        for word in words:
-            if word not in target_words:
-                target_words.add(word)
-
-    input_words = sorted(list(input_words))
-    target_words = sorted(list(target_words))
-    num_encoder_words = len(input_words)
-    num_decoder_words = len(target_words)
-
-    # 入力文と出力文それぞれで最大単語数計算
-    max_encoder_seq_length = max([len(words) for words in input_texts])
-    max_decoder_seq_length = max([len(words) for words in target_texts])
-
-    print('Number of samples:', len(input_texts))
-    print('Number of unique input words:', num_encoder_words)
-    print('Number of unique output words:', num_decoder_words)
-    print('Max sequence length for inputs:', max_encoder_seq_length)
-    print('Max sequence length for outputs:', max_decoder_seq_length)
-
-    # 単語にIDを割り振る
-    input_word_index = dict(
-        [(word, i) for i, word in enumerate(input_words)])
-    target_word_index = dict(
-        [(word, i) for i, word in enumerate(target_words)])
-
-    #input_textsは文の配列なので、単語の配列で構成された文の配列が必要？
-    #one-hot encodingになってるので分散表現に変更したい
-    encoder_input_data = np.zeros(
-        (len(input_texts), max_encoder_seq_length, num_encoder_words),
-        dtype='uint8')
-    decoder_input_data = np.zeros(
-        (len(input_texts), max_decoder_seq_length, num_decoder_words),
-        dtype='uint8')
-    decoder_target_data = np.zeros(
-        (len(input_texts), max_decoder_seq_length, num_decoder_words),
-        dtype='uint8')
-
-    # 文のインデックスと単語のインデックスと単語を格納するデータを作成
-    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-        for t, word in enumerate(input_text):
-            encoder_input_data[i, t, input_word_index[word]] = 1
-        for t, word in enumerate(target_text):
-            # decoder_target_data is ahead of decoder_input_data by one timestep
-            decoder_input_data[i, t, target_word_index[word]] = 1
-            if t > 0:
-                # decoder_target_data will be ahead by one timestep
-                # and will not include the start character.
-                decoder_target_data[i, t - 1, target_word_index[word]] = 1
-
-    # Define an input sequence and process it.
-    with tf.device("/cpu:0"):
-        encoder_inputs = Input(shape=(None, num_encoder_words))
-        encoder = LSTM(latent_dim, return_state=True)
-        _, state_h, state_c = encoder(encoder_inputs)
-        # We discard `encoder_outputs` and only keep the states.
-        encoder_states = [state_h, state_c]
-
-        # Set up the decoder, using `encoder_states` as initial state.
-        decoder_inputs = Input(shape=(None, num_decoder_words))
-        # We set up our decoder to return full output sequences,
-        # and to return internal states as well. We don't use the
-        # return states in the training model, but we will use them in inference.
-        decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-        decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
-                                            initial_state=encoder_states)
-        decoder_dense = Dense(num_decoder_words, activation='softmax')
-        decoder_outputs = decoder_dense(decoder_outputs)
-
-        # Define the model that will turn
-        # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-        model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-
-    n_split = int(encoder_input_data.shape[0]*0.8)
-    encoder_train, encoder_val = np.vsplit(encoder_input_data,[n_split])   #エンコーダインプットデータを訓練用と評価用に分割
-    decoder_train, decoder_val = np.vsplit(decoder_input_data,[n_split])   #デコーダインプットデータを訓練用と評価用に分割
-    target_train, target_val = np.vsplit(decoder_target_data,[n_split])   #ラベルデータを訓練用と評価用に分割
-    # Run training
-    model = multi_gpu_model(model, gpus=gpu_count)
-    model.compile(optimizer='rmsprop', loss=ppx) 
     row_train = encoder_train.shape[0]
     row_val = encoder_val.shape[0]
     n_batch = math.ceil(row_train/batch_size)
@@ -184,12 +188,12 @@ if args.mode == 'train':
         else:
             print('EarlyStopping')
             #break
-    json_string = model.to_json()
-    open(model_filename, 'w', encoding='utf-8').write(json_string)
+    #json_string = model.to_json()
+    #open(model_filename, 'w', encoding='utf-8').write(json_string)
     model.save_weights(weights_filename)
 elif args.mode == 'test':
-    json_string = open(model_filename,'r',encoding='utf-8').read()
-    model = model_from_json(json_string)
+    #json_string = open(model_filename,'r',encoding='utf-8').read()
+    #model = model_from_json(json_string)
     model.load_weights(weights_filename)
 # Define sampling models
 encoder_model = Model(encoder_inputs, encoder_states)
@@ -257,16 +261,27 @@ if args.mode=='train':
 elif args.mode=='test':
     print("会話テキストを入力してください\n")
     m = MeCab.Tagger("-Owakati")
-    input_words = []
-    input_seq = np.zeros((max_encoder_seq_length,num_encoder_words),dtype='float32')
-    while True:
+    #input_words = []
+    #input_seq = np.zeros((max_encoder_seq_length,num_encoder_words),dtype='float32')
+    for seq_index in range(10):
+        # Take one sequence (part of the training set)
+        # for trying out decoding.
+        input_seq = encoder_input_data[seq_index: seq_index + 1]
+        decoded_sentence = decode_sequence(input_seq)
+
+        print('-')
+        print('Input sentence:', ''.join(input_texts[seq_index]))
+        print('Decoded sentence:', decoded_sentence)
+    """while True:
+        input_seq = []
         input_text = input()  # 形態素解析前の平文 
-        input_words = m.parse(input_text).split(' '|"　")  # 分かち書き
+        input_words = m.parse(input_text).split()  # 分かち書き
         for i,word in enumerate(input_words):
-            input_seq = i, input_word_index[word] 
+            input_seq.append(input_word_index[word]) 
             # 文における単語のインデックス（登場順）と単語ID（既知語であれば）
             
         decoded_sentence = decode_sequence(input_seq)
         print('-')
-        print('Input sentence:', input_texts[seq_index])
+        print('Input sentence:', ''.join(input_texts[seq_index]))
         print('Decoded sentence:', decoded_sentence)
+    """
